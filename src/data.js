3023b1011fdcbe559e73b72fc7a11860bc83dfea
MataGitUniat <mikes0174@hotmail.com>
2022-06-14 08:29:44 -0500

    Routes & NavBar

---
+++ src/data.js
@@ -0,0 +1,21 @@
+let notes = [
+    {
+        id:1,
+        title:'Estudio',
+        description:'Terminar las tareas de Matematicas'
+    },
+    {
+        id:2,
+        title:'Tareas',
+        description:'Sacar a Pasear al Perro'
+    },
+    {
+        id:3,
+        title:'Jugar',
+        description:'Instalar Destiny 2 en la nueva PC'
+    },
+];
+
+export function getNotes(){
+    return notes;
+}
